50a36c34f31f0f42367eda72eeec6ca4d64d40f1
duy hoang <hoangtd.dev@gmail.com>
2025-02-19 17:49:59 +1100

    FE - Remove unuse path

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BibleComponent } from './components/bible/bible.component';
 
 @Component({
   selector: 'app-root',
--- ClientApp/src/app/app.config.ts
+++ ClientApp/src/app/app.config.ts
@@ -9,6 +9,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+
     provideStore({}),
     provideEffects([]),
   ],
