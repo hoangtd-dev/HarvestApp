77296094efea786970f30a6a15063dc9314d2ccb
duy hoang <hoangtd.dev@gmail.com>
2025-06-26 17:07:22 +0700

    update: UI adjustment for sidebar and header

--- ClientApp/tailwind.config.js
+++ ClientApp/tailwind.config.js
@@ -1,13 +1,13 @@
-const colors = require("tailwindcss/colors");
+const colors = require('tailwindcss/colors');
 
 module.exports = {
-  darkMode: "selector",
-  content: ["./src/**/*.{html,ts}"],
+  darkMode: 'selector',
+  content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {
       colors: {
-        black: "#0E0E11",
-        blackLight: "#18181C",
+        black: '#0E0E11',
+        blackLight: '#18181C',
         primary: colors.blue,
         neutral: colors.gray,
       },
