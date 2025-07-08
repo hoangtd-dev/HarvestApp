da1a7e0de002694424bb756453e7c2de03891134
duy hoang <hoangtd.dev@gmail.com>
2025-07-08 15:52:19 +0700

    [BE] update: add comment in auth middleware

--- ServerSide/src/middlewares/authMiddleware.ts
+++ ServerSide/src/middlewares/authMiddleware.ts
@@ -5,5 +5,9 @@ export const authMiddleware: RequestHandler = (
   res: Response,
   next: NextFunction
 ) => {
+  // const token = req.headers.authorization;
+  // if (!token) {
+  //   return res.status(401).json({ message: "Unauthorized" });
+  // }
   next();
 };
